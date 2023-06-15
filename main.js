a6054db7c110f30b0472224f5ae274de0a6e8ffc
human <matthew.j.cummings123@gmail.com>
2023-06-15 11:55:49 +1000

    nothing works

--- main.js
+++ main.js
@@ -7,4 +7,23 @@ function clickEvent(){
   function num(){
 document.getElementById("costs").innerText=document.getElementById("costs").innerText*1+event.target.innerText*1;
   }
-  //activate the collect button by seeing if all tiems are paid or else stay disabled
+//jquery count
+
+ $(document).ready(function () {
+        $('#btnadd').on('click', function () {
+            var n1 = parseInt($('#txtn1').val());
+            var n2 = parseInt($('#txtn2').val());
+			var n3 = parseInt($('#txtn3').val());
+			var n4 = parseInt($('#txtn4').val());
+            var r = n1 + n2 + n3 + n4;
+            return false;
+        });
+        $('#btnclear').on('click', function () {
+            $('#txtn1').val('1');
+            $('#txtn2').val('2');
+			$('#txtn3').val('3');
+			$('#txtn4').val('4');
+            $('#txtn1').focus();
+            return false;
+        });
+    });
