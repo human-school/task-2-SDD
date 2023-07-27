c7e4601c43801cb50ac14b0239af285975843201
human <matthew.j.cummings123@gmail.com>
2023-07-27 12:59:47 +1000

    money add
    money can be added into the system

--- main.js
+++ main.js
@@ -1,44 +1,52 @@
-
+//the text for the first snak option
 function showDiv1() {
    document.getElementById('content_caract_1').style.display = "block";
       document.getElementById('btnShow1').disabled = true;
    document.getElementById('btnHide1').disabled = false;
 }
+//the text for the second snak option
 function showDiv2() {
    document.getElementById('content_caract_2').style.display = "block";
       document.getElementById('btnShow2').disabled = true;
    document.getElementById('btnHide2').disabled = false;
 }
+//the text for the thrid snak option
 function showDiv3() {
    document.getElementById('content_caract_3').style.display = "block";
       document.getElementById('btnShow3').disabled = true;
    document.getElementById('btnHide3').disabled = false;
 }
+//the text for the forth snak option
 function showDiv4() {
    document.getElementById('content_caract_4').style.display = "block";
    document.getElementById('btnShow4').disabled = true;
    document.getElementById('btnHide4').disabled = false;
 }
+//the code that unlocks the select the firsts snak options
 function hideDiv1() {
    document.getElementById('content_caract_1').style.display = "none";
    document.getElementById('btnShow1').disabled = false;
    document.getElementById('btnHide1').disabled = true;
 }
+//the code that unlocks the select te second snak options
 function hideDiv2() {
    document.getElementById('content_caract_2').style.display = "none";
    document.getElementById('btnShow2').disabled = false;
    document.getElementById('btnHide2').disabled = true;
 }
+//the code that unlocks the select the thrid snak options
 function hideDiv3() {
    document.getElementById('content_caract_3').style.display = "none";
    document.getElementById('btnShow3').disabled = false;
    document.getElementById('btnHide3').disabled = true;
 }
+//the code that unlocks the select the forth snak options
 function hideDiv4() {
    document.getElementById('content_caract_4').style.display = "none";
    document.getElementById('btnShow4').disabled = false;
    document.getElementById('btnHide4').disabled = true;
 }
+//refreshes the seleted snak option without refreshing the page
 function hide() {
    document.getElementById('content_caract_1').style.display = "none";
    document.getElementById('content_caract_2').style.display = "none";
@@ -52,15 +60,15 @@ function hide1() {
   hide();
 }
 function hide2() {
-  hideDiv1();
+  hideDiv2();
   hide();
 }
 function hide3() {
-  hideDiv1();
+  hideDiv3();
   hide();
 }
 function hide4() {
-  hideDiv1();
+  hideDiv4();
   hide();
 }
 function show1() {
@@ -74,4 +82,20 @@ function show3() {
 }
 function show4() {
   showDiv4();
+}
+function money1() {
+  const input = document.getElementById("total");
+  input.value = Number(input.value) + 1;
+}
+function money2() {
+  const input = document.getElementById("total");
+  input.value = Number(input.value) + 2;
+}
+function money5() {
+  const input = document.getElementById("total");
+  input.value = Number(input.value) + 5;
+}
+function money10() {
+  const input = document.getElementById("total");
+  input.value = Number(input.value) + 10;
 }
